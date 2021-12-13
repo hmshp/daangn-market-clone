567ca96678de195355a8febb0005ada71d2a6724
Hyemi Son <hyemisonne@gmail.com>
2021-12-13 17:58:07 +0900

    Change price type
    Change price type from string to number

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import { products } from './data.js'
+import { products } from './data.js';
+import { useState } from 'react';
 
 export default function App() {
-  return (
-    <div>
-      <h1>상품 목록</h1>
-      <button type="submit">상품 추가</button>
-      {products.map(product =>
-        <div key={product.id}>
-          <h2>{product.name}</h2>
-          <img
-            src={product.imageUrl}
-            alt={product.name}
-            width={140}
-            height={140}
-          />
-          <ul> 
-            <li>{product.price}</li>
-            <li>{product.category}</li>
-            <li>{product.detail}</li>
-          </ul>
-        </div>)}
-    </div>
-  );
+
+    const [ShowForm, setShowForm] = useState(false);
+    function handleClick() {
+      setShowForm(!ShowForm);
+    }
+    
+    if(ShowForm) {
+      return (
+        <div>
+          <label for="title">제목:</label>
+          <input type="text" id="title"></input>
+    
+          <label for="photo-link">사진 링크:</label>
+          <input type="text" id="photo-link"></input>
+    
+          <label for="category">카테고리:</label>
+          <input type="text" id="category"></input>
+    
+          <label for="price">가격:</label>
+          <input type="number" id="price"></input>
+    
+          <label for="content">글 내용:</label>
+          <input type="text" id="content"></input>
+          <button onClick={handleClick}>홈으로 돌아가기</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>상품 목록</h1>
+          <button type="submit" onClick={handleClick}>상품 추가</button>
+          {products.map(product =>
+            <div key={product.id}>
+              <h2>{product.name}</h2>
+              <img
+                src={product.imageUrl}
+                alt={product.name}
+                width={140}
+                height={140}
+              />
+              <ul> 
+                <li>{product.price}</li>
+                <li>{product.category}</li>
+                <li>{product.detail}</li>
+              </ul>
+            </div>)}
+        </div>
+      );
+    }
+
+
 }
--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export const products = [{
   id: 'iphone-12',
   name: '아이폰 12',
-  price: '500,000원', /* 일단 문자열로 적었다 */
+  price: 500000,
   category: '전자기기',
   detail: '구매한 지 1년 미만, 미개봉',
   imageUrl: 'https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
 }, { 
   id: 'lululemon-pants',
   name: '룰루레몬 요가복',
-  price: '20,000원',
+  price: 20000,
   category: '의류',
   detail: '새 옷(한 번도 입지 않음)',
   imageUrl: 'https://images.unsplash.com/photo-1575052814086-f385e2e2ad1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
