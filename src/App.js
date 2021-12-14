3d0ca39c221c2abba79786ba66048b0df36331d0
Hyemi Son <hyemisonne@gmail.com>
2021-12-14 18:18:24 +0900

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
-import { products } from './data.js';
-import { useState } from 'react';
+import Home from './Home';
+import Form from './Form';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 export default function App() {
-
-    const [ShowForm, setShowForm] = useState(false);
-    function handleClick() {
-      setShowForm(!ShowForm);
-    }
-    
-    if(ShowForm) {
-      return (
-        <div>
-          <label htmlFor="title">제목:</label>
-          <input type="text" id="title"></input>
-    
-          <label htmlFor="photo-link">사진 링크:</label>
-          <input type="text" id="photo-link"></input>
-    
-          <label htmlFor="category">카테고리:</label>
-          <input type="text" id="category"></input>
-    
-          <label htmlFor="price">가격:</label>
-          <input type="number" id="price"></input>
-    
-          <label htmlFor="content">글 내용:</label>
-          <input type="text" id="content"></input>
-          <button onClick={handleClick}>홈으로 돌아가기</button>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1>상품 목록</h1>
-          <button type="submit" onClick={handleClick}>상품 추가</button>
-          {products.map(product =>
-            <div key={product.id}>
-              <h2>{product.name}</h2>
-              <img
-                src={product.imageUrl}
-                alt={product.name}
-                width={140}
-                height={140}
-              />
-              <ul> 
-                <li>{product.price}</li>
-                <li>{product.category}</li>
-                <li>{product.detail}</li>
-              </ul>
-            </div>)}
-        </div>
-      );
-    }
-
+  return (
+    <BrowserRouter>
+      <h1>홍당무 마켓</h1>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/form">상품 추가</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/form" element={<Form />} />
+      </Routes>
+    </BrowserRouter>
+  );
 
 }
