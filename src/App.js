ad7b257d28b2ee6ea07f8afcdef0c7c880c3ee08
Hyemi Son <hyemisonne@gmail.com>
2022-01-05 19:58:40 +0900

    Create buttons to add, modify, or delete products

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ export default function App() {
   return (
       <div className="home-container">
         <h1 className="title">홍당무 마켓</h1>
+        <div className="change-data-buttons">
+          <button className='add-item'>추가</button>
+          <button>수정 및 삭제</button>
+        </div>
         <div className="product-lists">
           {productLists}
         </div>
