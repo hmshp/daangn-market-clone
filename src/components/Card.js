8cacd3caebdfcfb8cf7a374f7fd27f0d6357590f
Hyemi Son <hyemisonne@gmail.com>
2022-01-05 21:10:26 +0900

    Add alternative text to images

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Card(props) {
     return (
       <div key={props.card.id} className="products-container">
-        <img className="product-image" src={props.card.imageUrl} />
+        <img className="product-image" src={props.card.imageUrl} alt={props.card.name} />
         <div className="product-description">
           <h2>{props.card.name}</h2>
           <p>{props.card.category}</p>
