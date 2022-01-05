9692b262fdd87937c3967e34e1f235067790629c
Hyemi Son <hyemisonne@gmail.com>
2022-01-05 20:32:12 +0900

    Move Card component to seperate file from App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import Home from "./Home";
 import Form from "./Form";
 import { products } from "./data";
 import "./style.css"
+import Card from "./Card"
 
 export default function App() {
-  const productLists = products.map(product => {
-    return (
-      <div class="products-container">
-        <img className="product-image" src={product.imageUrl} />
-        <div className="product-description">
-          <h2>{product.name}</h2>
-          <p>{product.category}</p>
-          <h4>{product.price}</h4>
-        </div>
-      </div>
-    )
-  })
   return (
       <div className="home-container">
         <h1 className="title">홍당무 마켓</h1>
@@ -26,7 +15,7 @@ export default function App() {
           <button>수정 및 삭제</button>
         </div>
         <div className="product-lists">
-          {productLists}
+          <Card />
         </div>
       </div>
   );
---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { products } from "./data"
+
+export default function Card() {
+  const productLists = products.map(product => {
+    return (
+      <div class="products-container">
+        <img className="product-image" src={product.imageUrl} />
+        <div className="product-description">
+          <h2>{product.name}</h2>
+          <p>{product.category}</p>
+          <h4>{product.price}</h4>
+        </div>
+      </div>
+    )
+  })
+  return productLists
+}
