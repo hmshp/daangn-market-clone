eb9f9872f09a073e863ae75672583d5ce25d5395
Hyemi Son <hyemisonne@gmail.com>
2022-01-05 20:35:05 +0900

    Create Header component in new file

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import Form from "./Form";
 import { products } from "./data";
 import "./style.css"
 import Card from "./Card"
+import Header from "./Header"
 
 export default function App() {
   return (
       <div className="home-container">
-        <h1 className="title">홍당무 마켓</h1>
-        <div className="change-data-buttons">
-          <button className='add-item'>추가</button>
-          <button>수정 및 삭제</button>
-        </div>
+        <Header />
         <div className="product-lists">
           <Card />
         </div>
