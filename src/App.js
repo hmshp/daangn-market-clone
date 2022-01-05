8a7cde1cfd3f7da916df1b876940fb4ef8b113f8
Hyemi Son <hyemisonne@gmail.com>
2022-01-05 19:44:03 +0900

    Apply grid layout to product list

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import React from "react";
 import Home from "./Home";
 import Form from "./Form";
 import { products } from "./data";
+import "./style.css"
 
 export default function App() {
+  const productLists = products.map(product => {
+    return (
+      <div class="products-container">
+        <img className="product-image" src={product.imageUrl} />
+        <div className="product-description">
+          <h2>{product.name}</h2>
+          <p>{product.category}</p>
+          <h4>{product.price}</h4>
+        </div>
+      </div>
+    )
+  })
   return (
       <div className="home-container">
         <h1 className="title">홍당무 마켓</h1>
-        <div>
+        <div className="product-lists">
+          {productLists}
         </div>
       </div>
   );
