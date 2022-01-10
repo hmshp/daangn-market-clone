24e4266328e4c60e9cdae6cbb98e18fa6b33af43
Hyemi Son <hyemisonne@gmail.com>
2022-01-10 12:20:11 +0900

    Feat: add onClick event to button to change state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react"
 
-export default function Header() {
+export default function Header(props) {
+  //Header 컴포넌트 안에 있는 버튼 element에 onClick={props.handleClick} 적기. 이때의 onClick은 진짜 이벤트 리스너다! img 태그는 DOM element니까
+  
   return (
     <header>
-      <h1 className="title">홍당무 마켓</h1>
+      <h1 className="header-title">홍당무 마켓</h1>
       <div className="change-data-buttons">
-        <button className='add-item'>추가</button>
+        <button onClick={props.handleClick} className='add-item'>추가</button>
         <button>수정 및 삭제</button>
       </div>
     </header>
