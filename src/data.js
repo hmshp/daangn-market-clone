8bbf2f195dee66dbdbc30f2c7b2e35d2ab38dd07
Hyemi Son <hyemisonne@gmail.com>
2022-01-10 12:15:45 +0900

    Style: add one more object to data array to see grid layout on screen more clearly

--- src/data.js
+++ src/data.js
@@ -20,4 +20,11 @@ export const products = [{
   category: '장난감',
   detail: '귀여운 갈색 테디베어입니다.',
   imageUrl: 'https://images.unsplash.com/photo-1602734846297-9299fc2d4703?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80',
+}, { 
+  id: 'teddy-bear2',
+  name: '곰인형',
+  price: 5000,
+  category: '장난감',
+  detail: '귀여운 갈색 테디베어입니다.',
+  imageUrl: 'https://images.unsplash.com/photo-1602734846297-9299fc2d4703?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80',
 }]
