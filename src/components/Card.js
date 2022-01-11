67a387d18978e51bac5ea613f160278e7d5b41d9
Hyemi Son <hyemisonne@gmail.com>
2022-01-11 12:41:17 +0900

    Style: add .product-title to style element

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ export default function Card(props) {
       <div key={props.card.id} className="products-container">
         <img className="product-image" src={props.card.imageUrl} alt={props.card.name} />
         <div className="product-description">
-          <h2>{props.card.name}</h2>
+          <h2 className="product-title">{props.card.name}</h2>
           <p>{props.card.category}</p>
           <h4>{props.card.price}</h4>
         </div>
