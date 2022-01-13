7ddd972cf6d17420576d151b41f4146259f3ab6f
Hyemi Son <hyemisonne@gmail.com>
2022-01-13 11:16:45 +0900

    Style: change name of Form state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,50 +1,73 @@
-import React from "react"
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 export default function Form() {
-  const [productInfo, setProductInfo] = useState({
+  const [formData, setFormData] = useState({
     title: "",
     photoLink: "",
     category: "",
     price: 0,
     content: "",
   });
-  const onChange = (event) => setProductInfo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-  }
+  //위 object의 state 올바르게 관리할 수 있게 코드 수정하기
+
   return (
     <div className="form-container">
       <form className="form" onSubmit={onSubmit}>
-      <h2 className="form-global-title">상품 추가</h2>
-          <div className="form-inputs-container">
-            <div className="form-input-wrapper">
-              <label htmlFor='title'>제목: </label>
-              <input onChange={onChange} defaultValue={productInfo.title} type="text" id="title" />
-            </div>
-            
-            <div className="form-input-wrapper">
-              <label htmlFor='photo-link'>사진 링크:</label>
-              <input onChange={onChange} defaultValue={productInfo.photoLink} type="text" id="photo-link" />
-            </div>
+        <h2 className="form-global-title">상품 추가</h2>
+        <div className="form-inputs-container">
+          <div className="form-input-wrapper">
+            <label htmlFor="title">제목: </label>
+            <input
+              onChange={onChange}
+              defaultValue={productInfo.title}
+              type="text"
+              id="title"
+            />
+          </div>
 
-            <div className="form-input-wrapper">
-              <label htmlFor='category'>카테고리:</label>
-              <input onChange={onChange} defaultValue={productInfo.category} type="text" id="category" />
-            </div>
+          <div className="form-input-wrapper">
+            <label htmlFor="photo-link">사진 링크:</label>
+            <input
+              onChange={onChange}
+              defaultValue={productInfo.photoLink}
+              type="text"
+              id="photo-link"
+            />
+          </div>
 
-            <div className="form-input-wrapper">
-              <label htmlFor='price'>가격:</label>
-              <input onChange={onChange} defaultValue={productInfo.price} type="number" id="price" />
-            </div>
+          <div className="form-input-wrapper">
+            <label htmlFor="category">카테고리:</label>
+            <input
+              onChange={onChange}
+              defaultValue={productInfo.category}
+              type="text"
+              id="category"
+            />
+          </div>
 
-            <div className="form-input-wrapper">
-              <label htmlFor='content'>글 내용:</label>
-              <input onChange={onChange} defaultValue={productInfo.content} type="text" id="content" />
-            </div>
+          <div className="form-input-wrapper">
+            <label htmlFor="price">가격:</label>
+            <input
+              onChange={onChange}
+              defaultValue={productInfo.price}
+              type="number"
+              id="price"
+            />
+          </div>
 
-            <button className="form-add-button">상품 추가</button>
-          </div>      
+          <div className="form-input-wrapper">
+            <label htmlFor="content">글 내용:</label>
+            <input
+              onChange={onChange}
+              defaultValue={productInfo.content}
+              type="text"
+              id="content"
+            />
+          </div>
+
+          <button className="form-add-button">상품 추가</button>
+        </div>
       </form>
     </div>
   );
