09de349cae193f411ca02e0834cb3c90588b1762
Hyemi Son <hyemisonne@gmail.com>
2022-01-13 17:23:59 +0900

    Feat: let state control input values so that state can be single source of truth

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,7 +10,6 @@ export default function Form() {
     content: "",
   });
 
-  //위 object의 state 올바르게 관리할 수 있게 아래 코드 수정하기
   function handleChange(event) {
     // console.log(event);
     setFormData((prevFormData) => {
@@ -28,29 +27,49 @@ export default function Form() {
         <div className="form-inputs-container">
           <div className="form-input-wrapper">
             <label htmlFor="title">제목: </label>
-            <input onChange={handleChange} type="text" id="title" />
+            <input
+              onChange={handleChange}
+              type="text"
+              id="title"
+              value={formData.title}
+            />
           </div>
-
           <div className="form-input-wrapper">
             <label htmlFor="photo-link">사진 링크:</label>
-            <input onChange={handleChange} type="text" id="photo-link" />
+            <input
+              onChange={handleChange}
+              type="text"
+              id="photo-link"
+              value={formData.photoLink}
+            />
           </div>
-
           <div className="form-input-wrapper">
             <label htmlFor="category">카테고리:</label>
-            <input onChange={handleChange} type="text" id="category" />
+            <input
+              onChange={handleChange}
+              type="text"
+              id="category"
+              value={formData.category}
+            />
           </div>
-
           <div className="form-input-wrapper">
             <label htmlFor="price">가격:</label>
-            <input onChange={handleChange} type="number" id="price" />
+            <input
+              onChange={handleChange}
+              type="number"
+              id="price"
+              value={formData.price}
+            />
           </div>
-
           <div className="form-input-wrapper">
             <label htmlFor="content">글 내용:</label>
-            <input onChange={handleChange} type="text" id="content" />
+            <input
+              onChange={handleChange}
+              type="text"
+              id="content"
+              value={formData.content}
+            />
           </div>
-
           <button className="form-add-button">상품 추가</button>
         </div>
       </form>
