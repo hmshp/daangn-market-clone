ac2472979781798f82394d51832325f0f9c81c55
Hyemi Son <hyemisonne@gmail.com>
2022-01-13 11:50:26 +0900

    Fix: set state object in proper way w/ ... syntax Previous code was how we set state of number or string

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,61 +9,46 @@ export default function Form() {
     price: 0,
     content: "",
   });
-  //위 object의 state 올바르게 관리할 수 있게 코드 수정하기
+
+  //위 object의 state 올바르게 관리할 수 있게 아래 코드 수정하기
+  function handleChange(event) {
+    // console.log(event);
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
 
   return (
     <div className="form-container">
-      <form className="form" onSubmit={onSubmit}>
+      <form className="form">
         <h2 className="form-global-title">상품 추가</h2>
         <div className="form-inputs-container">
           <div className="form-input-wrapper">
             <label htmlFor="title">제목: </label>
-            <input
-              onChange={onChange}
-              defaultValue={productInfo.title}
-              type="text"
-              id="title"
-            />
+            <input onChange={handleChange} type="text" id="title" />
           </div>
 
           <div className="form-input-wrapper">
             <label htmlFor="photo-link">사진 링크:</label>
-            <input
-              onChange={onChange}
-              defaultValue={productInfo.photoLink}
-              type="text"
-              id="photo-link"
-            />
+            <input onChange={handleChange} type="text" id="photo-link" />
           </div>
 
           <div className="form-input-wrapper">
             <label htmlFor="category">카테고리:</label>
-            <input
-              onChange={onChange}
-              defaultValue={productInfo.category}
-              type="text"
-              id="category"
-            />
+            <input onChange={handleChange} type="text" id="category" />
           </div>
 
           <div className="form-input-wrapper">
             <label htmlFor="price">가격:</label>
-            <input
-              onChange={onChange}
-              defaultValue={productInfo.price}
-              type="number"
-              id="price"
-            />
+            <input onChange={handleChange} type="number" id="price" />
           </div>
 
           <div className="form-input-wrapper">
             <label htmlFor="content">글 내용:</label>
-            <input
-              onChange={onChange}
-              defaultValue={productInfo.content}
-              type="text"
-              id="content"
-            />
+            <input onChange={handleChange} type="text" id="content" />
           </div>
 
           <button className="form-add-button">상품 추가</button>
