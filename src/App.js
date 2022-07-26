940208a3f2ccaa342be682a5d2db28566544e378
hmshp <hyemisonne@gmail.com>
2022-07-26 21:19:41 +0900

    Add build folder

--- src/App.js
+++ src/App.js
@@ -76,27 +76,5 @@ export default function App() {
     </div>
   );
 }
-//상품 클릭하면 Detail 컴포넌트 뜨게 하기. 어떻게?
 
-//1. App에서 Card 컴포넌트 리턴할 때 handleEdit props도 보내기
-//2. Card에서 onClick=props.handleEdit 추가
-//3. App에 handleEdit 만들어놓기. 내용은:
-//   [editMode, setEditMode] = useState(false);이라는 state 새로 만들고
-//   handleEdit editMode true로 만들기
-//4. main-page 섹션에서 isShown? 위에 조건문 먼저 달기. 어떤 조건문이냐면:
-//   (editMode이 true면) -> Detail 렌더링
-//   else(showDetail이 false면) -> 그때 isShown 조건문 달기
-//   (isShown 이름도 나중에 addItem 같이 구체적인 의미가 담긴 이름으로 바꾸기)
-
-//5. Detail 컴포넌트 만들기 - Form과 같은 폼 넣기. 값은 클릭한 상품과 같은 값.
-//  그러려면 어떤 상품을 클릭했는지 알아야 한다. 어떻게? id로.
-
-// - props로 폼 항목, 수정, 삭제 버튼 만들기
-
-
-
-//Detail 컴포넌트 뜨면 폼 수정한 다음 수정내용 반영 or 삭제
-//수정한 내용 상태를 파악하려면? 새로운 state 있어야 함.
-//App에 [detailData, setDetailData]라는 state 새로 만들기
-//초기값은 products 중에서 id가  
 
