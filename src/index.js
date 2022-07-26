38e3b56f3cc8ec78b4dba464624e3152991118b9
hmshp <hyemisonne@gmail.com>
2022-07-26 22:14:12 +0900

    Add files to gh-pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /* 이 파일은  리액트 어플리케이션을 렌더링해 줄 뿐*/
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App /> 
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter basename="/">
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
